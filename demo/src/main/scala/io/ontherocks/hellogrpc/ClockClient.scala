07533ac62e446616cf9a3db916e9e4b0acd6f81f
Petra Bierleutgeb <petra.bierleutgeb@protonmail.ch>
2018-05-18 19:33:26 +0200

    Keep application running until channel is terminated

--- demo/src/main/scala/io/ontherocks/hellogrpc/ClockClient.scala
+++ demo/src/main/scala/io/ontherocks/hellogrpc/ClockClient.scala
@@ -16,9 +16,15 @@
 
 package io.ontherocks.hellogrpc
 
-import io.grpc.ManagedChannelBuilder
+import java.util.concurrent.TimeUnit
+
+import io.grpc.{ ManagedChannel, ManagedChannelBuilder }
 import io.grpc.stub.StreamObserver
 import io.ontherocks.hellogrpc.clock.{ ClockGrpc, TimeRequest, TimeResponse }
+
+import scala.concurrent.{ Await, Promise }
+import scala.concurrent.duration._
+import scala.util.{ Failure, Success, Try }
 
 object ClockClient {
 
@@ -26,14 +32,34 @@ object ClockClient {
     val channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext(true).build
     val client  = ClockGrpc.stub(channel)
 
-    val observer: StreamObserver[TimeResponse] = new StreamObserver[TimeResponse] {
-      def onError(t: Throwable): Unit = println(s"ON_ERROR: $t")
-      def onCompleted(): Unit         = println("ON_COMPLETED")
-      def onNext(response: TimeResponse): Unit =
-        println(s"ON_NEXT: Received current time ms: ${response.currentTime}")
-    }
+    def observer(p: Promise[String]): StreamObserver[TimeResponse] =
+      new StreamObserver[TimeResponse] {
+        def onError(t: Throwable): Unit = {
+          println(s"ON_ERROR: $t")
+          p.complete(tryAwaitTermination(channel, "received onError"))
+        }
+        def onCompleted(): Unit = {
+          println("ON_COMPLETED")
+          p.complete(tryAwaitTermination(channel, "received onComplete"))
+        }
+        def onNext(response: TimeResponse): Unit =
+          println(s"ON_NEXT: Received current time ms: ${response.currentTime}")
+      }
 
-    client.streamTime(TimeRequest(), observer)
+    val terminated = Promise[String]
+    client.streamTime(TimeRequest(), observer(terminated))
+
+    val shutdownReason = Await.result(terminated.future, Duration.Inf)
+    println(s"Successfully shutdown application. Reason: $shutdownReason")
+  }
+
+  private def tryAwaitTermination(channel: ManagedChannel, reason: String): Try[String] = {
+    channel.shutdown()
+    if (channel.awaitTermination(5, TimeUnit.SECONDS)) {
+      Success(reason)
+    } else {
+      Failure(new RuntimeException("Could not terminate channel."))
+    }
   }
 
 }
